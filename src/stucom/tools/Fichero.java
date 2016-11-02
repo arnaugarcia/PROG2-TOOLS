6ca622c22f5a3180eb9f1154a1c4bb5c697d69fc
Arnau García <arnau.garcia.gallego@gmail.com>
2016-11-02 17:29:18 +0100

    Añadido pedirBoolean

--- src/stucom/tools/Fichero.java
+++ src/stucom/tools/Fichero.java
@@ -1,3 +1,7 @@
+/*
+ * Clase para guardar objetos en fichero xml
+ * Es genérica para cualquier tipo de Object
+ */
 package stucom.tools;
 
 import java.beans.XMLDecoder;
@@ -8,40 +12,43 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 /**
- * Created by Arnau on 05/10/16.
+ *
+ * @author mfontana
  */
 public class Fichero {
 
-    //Atributo para guardar el nombre del fichero
+    // Atributo para guardar el nombre del fichero
     private String nombre;
 
     public Fichero(String nombre) {
         this.nombre = nombre;
     }
 
-    //Graba cualquier objeto JavaBean que le pasen
-    public void grabar(Object o){
+    // Graba cualquier objeto JavaBean que le pasen
+    public void grabar(Object o) {
         try {
-            //Instanciamos objeto para codificar en XML y escribirlo en el fichero
+            // Instanciamos objeto para codificar el XML y escribirlo en el fichero
             XMLEncoder codificador = new XMLEncoder(new FileOutputStream(new File(nombre)));
-            //Escribimos el objeto en el fichero
+            // Escribimos el objeto en el fichero
             codificador.writeObject(o);
-            //Cerramos el canal
+            // Cerramos el canal
             codificador.close();
         } catch (FileNotFoundException ex) {
             System.out.println("Error no se ha podido grabar: " + ex.getMessage());
         }
     }
 
-    //Lee un objeto JavaBean de un fichero XML y lo devuleve
+    // Lee un objeto JavaBean de un fichero XML y lo devuelve
     // Devuelve NULL en caso de que el fichero no exista
-    public Object leer(){
-        try{
+    public Object leer() {
+        try {
             XMLDecoder decodificador = new XMLDecoder(new FileInputStream(new File(nombre)));
             return decodificador.readObject();
-        }catch (FileNotFoundException ex){
-            System.out.println("No existe el fichero todavía " + ex.getMessage());
+        } catch (FileNotFoundException ex) {
+            System.out.println("No existe el fichero todavía.");
             return null;
         }
+
     }
+
 }
